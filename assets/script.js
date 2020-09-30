90693e46c9d4cd1f6cfdfbe4d26b6398a076f1a9
Brian Lee <brianlee@Brians-MBP.hsd1.ca.comcast.net>
2020-09-29 21:53:10 -0700

    button click

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,24 @@
 var q = $('#cityName');
-var queryUrl = "api.openweathermap.org/data/2.5/forecast?q="+q+"&appid=1f8d3642112c88ffeef49b457df55b08";
+var btn = $('#goBtn');
+var cityName = $('#cityName');
+var cityDisplay = $('#cityDisplay');
+var history = $('searchHistory');
+
+$("#goBtn").on("click", function (event) {
+    event.preventDefault();
+    q = cityName.val();
+    var queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + q + "&appid=1f8d3642112c88ffeef49b457df55b08";
+    cityDisplay.text(cityName.val());
+    var historyDiv = $("<div>").text(cityName.val());
+    //history.text(historyDiv);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        var temp = response.list.main.temp;
+        console.log(temp);
+
+    });
+
+});
